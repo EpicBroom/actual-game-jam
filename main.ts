7b18be93478572b1a8ff714dd4f76b66ec08f43a
EpicBroom <tim.bourderau@challoners.org>
2025-11-13 08:41:18 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,33 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    mainCharacter.vy = -100
+})
+function defineCharacters () {
+	
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
+let mainCharacter: Sprite = null
+let Gravity = 20
+mainCharacter = sprites.create(img`
+    3 3 3 . . 3 . 3 . 3 3 3 . 3 3 . 
+    3 . . . . 3 . 3 . . 3 . . 3 . 3 
+    3 3 3 . . 3 3 . . . 3 . . 3 3 . 
+    . . 3 . . 3 . 3 . . 3 . . 3 . 3 
+    3 3 3 . . 3 . 3 . 3 3 3 . 3 3 . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . 3 3 . . 3 3 3 . . . . 
+    . . . . . 3 . 3 . . 3 . . . . . 
+    . . . . . 3 3 . . . 3 . . . . . 
+    . . . . . 3 . 3 . . 3 . . . . . 
+    . . . . . 3 3 . . 3 3 3 . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . 3 3 . . 3 3 3 . . . . 
+    . . . . . 3 . 3 . . 3 . . . . . 
+    . . . . . 3 . 3 . . 3 . . . . . 
+    . . . . . 3 3 . . 3 3 3 . . . . 
+    `, SpriteKind.Player)
+mainCharacter.setVelocity(500, 300)
 forever(function () {
-    game.splash("Currently editing")
+    mainCharacter.setBounceOnWall(true)
 })
